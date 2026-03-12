05d012b6e1007fbe1345788e7be3b0ea399047f4
asalnikov <asalnikov@megamarket.ru>
2026-03-12 09:34:54 +0300

    change user id calls and type

--- app/infrastructure/persistence/models/todo.py
+++ app/infrastructure/persistence/models/todo.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, Boolean, ForeignKey, DateTime, func
+from sqlalchemy import String, Integer, Boolean, ForeignKey, DateTime, func, UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from typing import Optional
@@ -17,5 +17,4 @@ class TodoORM(Base, AsyncAttrs):
     created_at: Mapped[Optional[DateTime]] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
-    user_id: Mapped[str] = mapped_column(ForeignKey("users.id"), nullable=False)
-    owner: Mapped["UserORM"] = relationship(back_populates="todos")
+    user_id: Mapped[str] = mapped_column(String(255), nullable=False)
